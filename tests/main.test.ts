1654fb8ce7aaabd0ec36dddcda9f4b045003fcfb
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2025-02-24 22:09:09 +0100

    Add test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -15,3 +15,16 @@ Deno.test("basic usage", () => {
   const resultStr = decoder.decode(result);
   expect(resultStr).toEqual("Hello librsync World !");
 });
+
+Deno.test("Copy file", () => {
+  const remoteFile = new Uint8Array([]);
+  const localFile = new Uint8Array([1, 2, 3, 4, 5]);
+
+  const sig = signature(remoteFile, 1024, 32);
+  expect([...sig]).toEqual([114, 115, 1, 54, 0, 0, 4, 1, 0, 0, 0, 0]);
+  const patch = diff(sig, localFile);
+  expect([...patch]).toEqual([114, 115, 2, 54, 5, 1, 2, 3, 4, 5, 0]);
+  const result = apply(remoteFile, patch);
+  expect([...result]).toEqual([...localFile]);
+  expect([...result]).toEqual([1, 2, 3, 4, 5]);
+});
